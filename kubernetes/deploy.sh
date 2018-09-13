c994d5829b74ac34748efd5a6fca0af3a9828f2b
Fabio Ribeiro <fdcr@student.dei.uc.pt>
2018-09-13 19:33:52 +0100

    Script updated

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#helm install --name my-kafka incubator/kafka
-
 cd zipkin/
 docker build -t fcribeiro/zipkin-emp .
 docker push fcribeiro/zipkin-emp
@@ -9,7 +7,5 @@ docker push fcribeiro/zipkin-emp
 cd ..
 
 kubectl create -f zipkin_emp.yaml
-
-kubectl create -f songs_deployment.yaml
 
 kubectl get services
